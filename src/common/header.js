104607ab6945ed29f14ff4475f8a050577143a2a
Mehrdad Nourbakhsh <m.nourbakhsh75@gmail.com>
2019-04-03 19:33:58 +0430

    add logo

--- src/common/header.js
+++ src/common/header.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import '../style/header.scss'
 import { Navbar } from 'react-bootstrap';
+import logo from '../assets/logo/logo v2.png'
 
 const message = {
     account : 'حساب کاربری',
     exit : 'خروج'
 }
+// class Logo extends Component {
+//     render(){
+//         return(
+//             <div className="logo">
+//                 <a href="/"><img src={logo} alt="LOGO" className="logoPic"/></a>
+//             </div>
+//         );
+//     }
+// }
 
 class Header extends Component {
     render(){
         return(
             <Navbar className="nav" expand="lg">
-                <Navbar.Brand href="#home">{message.account}</Navbar.Brand>
+                <Navbar.Brand href="#home"><img alt="logo icon" src={logo}/></Navbar.Brand>
+                <Navbar.Brand href="#account">{message.account}</Navbar.Brand>
                 <Navbar.Brand href="#exit">{message.exit}</Navbar.Brand>
             </Navbar>
         );
