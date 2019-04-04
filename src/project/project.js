a6396544f43e94d713cb2a632a908308221805d7
Mehrdad Nourbakhsh <m.nourbakhsh75@gmail.com>
2019-04-04 09:01:47 +0430

    project title

--- src/project/project.js
+++ src/project/project.js
@@ -2,12 +2,46 @@ import React, { Component } from 'react';
 import '../style/project.scss'
 import Header from '../common/Header'
 import Footer from '../common/Footer'
+import {Jumbotron ,Container, Row ,Col,Image} from 'react-bootstrap'
+
+const vars = {
+    projectImage : 'https://cdn4.vectorstock.com/i/1000x1000/31/48/software-developer-and-programmer-vector-10673148.jpg',
+    title : 'پروژه طراحی سایت جاب‌اونجا'
+}
+class TopBar extends Component {
+    render(){
+        return(
+            <div className="blueLine"></div>
+        );
+    }
+}
+
+class ProjectContainer extends Component {
+    render(){
+        return(
+            <Jumbotron className="projectContainer align-items-center">
+                <Container fluid className="cont">
+                    <Row className="info">
+                        <Col xs={6} md={4} className="profile">
+                            <Image src={vars.projectImage} fluid></Image>
+                        </Col>
+                        <Col xs={6} md={8} className="projectInf">
+                        <span className="title">{vars.title}</span>
+                        </Col>
+                    </Row>
+                </Container>
+            </Jumbotron>
+        );
+    }
+}
+
 class project extends Component {
     render(){
         return(
             <div className="project">
                 <Header/>
-                
+                <TopBar/>
+                <ProjectContainer/>
                 <Footer/>
             </div>
         );
